5209e7a0b4fd2c5e0835ea1c45ace78296a1c554
ProhtMeyhet <sebastian@prohtmeyhet.de>
2016-08-18 15:57:51 +0200

    minor abstractions in filesystem/mk

--- filesystem/mk/library.go
+++ filesystem/mk/library.go
@@ -125,18 +125,3 @@ func Directory(input *Input) (exitCode uint8) {
 
 	return
 }
-
-// create common directory structes (files, directorys and links)
-func Mk(input *Input) (exitCode uint8) {
-	switch {
-	case input.Link:
-		return Link(input)
-	case input.Temporary:
-		return Temporary(input)
-	case input.File:
-		return File(input)
-	default:
-		return Directory(input)
-	}
-
-}
