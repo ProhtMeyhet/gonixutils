0e2dbedd2dc2ebdbb8ed5b70a1f92ad7ff28c7b6
ProhtMeyhet <sebastian@prohtmeyhet.de>
2017-02-11 17:24:36 +0100

    sleep: add some more stamps

--- miscellaneous/sleep/tools.go
+++ miscellaneous/sleep/tools.go
@@ -44,6 +44,30 @@ func ParseUntil(unparsed string) (format string, result time.Time, e error) {
 		result = until; format = STAMP103; date =true
 	} else if until, e := time.ParseInLocation(STAMP104, unparsed, now.Location()); e == nil {
 		result = until; format = STAMP104; date = true
+	} else if until, e := time.ParseInLocation(STAMP105, unparsed, now.Location()); e == nil {
+		result = until; format = STAMP105; date = true
+	} else if until, e := time.ParseInLocation(STAMP106, unparsed, now.Location()); e == nil {
+		result = until; format = STAMP106; date = true
+	} else if until, e := time.ParseInLocation(STAMP107, unparsed, now.Location()); e == nil {
+		result = until; format = STAMP107; date = true
+	} else if until, e := time.ParseInLocation(STAMP108, unparsed, now.Location()); e == nil {
+		result = until; format = STAMP108; date = true
+	} else if until, e := time.ParseInLocation(STAMP109, unparsed, now.Location()); e == nil {
+		result = until; format = STAMP109; date = true
+	} else if until, e := time.ParseInLocation(STAMP110, unparsed, now.Location()); e == nil {
+		result = until; format = STAMP110; date = true
+	} else if until, e := time.ParseInLocation(STAMP111, unparsed, now.Location()); e == nil {
+		result = until; format = STAMP111; date = true
+	} else if until, e := time.ParseInLocation(STAMP112, unparsed, now.Location()); e == nil {
+		result = until; format = STAMP112; date = true
+	} else if until, e := time.ParseInLocation(STAMP113, unparsed, now.Location()); e == nil {
+		result = until; format = STAMP113; date = true
+	} else if until, e := time.ParseInLocation(STAMP114, unparsed, now.Location()); e == nil {
+		result = until; format = STAMP114; date = true
+	} else if until, e := time.ParseInLocation(STAMP115, unparsed, now.Location()); e == nil {
+		result = until; format = STAMP115; date = true
+	} else if until, e := time.ParseInLocation(STAMP116, unparsed, now.Location()); e == nil {
+		result = until; format = STAMP116; date = true
 	} else {
 		e = errors.New(fmt.Sprintf("cannot parse '%v' as until time!", unparsed))
 		return format, result, e
