fa9e594d0544878bc952ef4be7c5c72fe3b71fde
ProhtMeyhet <sebastian@prohtmeyhet.de>
2018-08-22 05:12:53 +0200

    signal: use parallel.Work

--- process/signal/library.go
+++ process/signal/library.go
@@ -5,24 +5,30 @@ import(
 	"os"
 
 	"github.com/ProhtMeyhet/gonixutils/library/abstract"
+
+	"github.com/ProhtMeyhet/libgosimpleton/parallel"
 )
 
 func Signal(input *Input) (exitCode uint8) {
-	for _, processId := range input.ProcessIds {
-		process, e := os.FindProcess(processId)
-		if abstract.PrintErrorWithError(e, input.Stderr, "error: %v") {
-			exitCode = abstract.ERROR_GENERIC
-			continue
+	work := parallel.NewIntsFeeder(input.ProcessIds...)
+
+	work.Start(func() {
+		for processId := range work.Talk {
+			process, e := os.FindProcess(processId)
+			if abstract.PrintErrorWithError(e, input.Stderr, "error: %v") {
+				exitCode = abstract.ERROR_GENERIC
+				continue
+			}
+
+			if input.Verbose { fmt.Fprintf(input.Stderr, "signaling: '%v' with %v\n",
+							processId, SignalToString(input.Signal) ) }
+
+			e = process.Signal(input.Signal)
+			if abstract.PrintErrorWithError(e, input.Stderr, "error: %v") {
+				exitCode = abstract.ERROR_GENERIC
+			}
 		}
+	})
 
-		if input.Verbose { fmt.Fprintf(input.Stderr, "signaling: '%v' with %v\n",
-						processId, SignalToString(input.Signal) ) }
-
-		e = process.Signal(input.Signal)
-		if abstract.PrintErrorWithError(e, input.Stderr, "error: %v") {
-			exitCode = abstract.ERROR_GENERIC
-		}
-	}
-
-	return
+	work.Wait(); return
 }
